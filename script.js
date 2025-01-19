005fb47c1ee6ea9816c090c92eb195a2d4ff6fc1
nagamanichirithoti <nagamanichirithoti9@gmail.com>
2025-01-19 13:21:00 +0530

    hello

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
-// Function to get all posts
 async function getPosts() {
-  const response = await fetch("http://localhost:3000/posts");
-  const posts = await response.json();
-  console.log(posts);
-}
-
-getPosts();
-
-// Function to add a new post
-async function addPost() {
-  const response = await fetch("http://localhost:3000/posts", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ title: "New Post", price: 2000 })
-  });
-  const post = await response.json();
-  console.log(post);
-}
-
-addPost();
-
-// Function to update the post with id 1 (use the correct URL `/posts/1`)
-async function updatePost() {
-  const response = await fetch("http://localhost:3000/posts/1", {  // Use `posts/1` instead of `posts`
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ title: "Updated Post 1", price: 1000 })
-  });
-  const post = await response.json();
-  console.log(post);
-}
-
-updatePost();
-
-// Function to delete the post with id 1 (use the correct URL `/posts/1`)
-async function deletePost() {
-  await fetch("http://localhost:3000/posts/1", {  // Use `posts/1` instead of `posts`
-    method: "DELETE"
-  });
-  console.log("Post deleted");
-}
-
-deletePost();
+    const response = await fetch("http://localhost:3000/posts");
+    const posts = await response.json();
+    console.log(posts);
+  }
+  
+  getPosts();
+  async function addPost() {
+    const response = await fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: "New Post", price: 2000 })
+    });
+    const post = await response.json();
+    console.log(post);
+  }
+  
+  addPost();
+  async function updatePost() {
+    const response = await fetch("http://localhost:3000/posts", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: "Updated Post 1", price:1000 })
+    });
+    const post = await response.json();
+    console.log(post);
+  }
+  
+  updatePost();
+  async function deletePost() {
+    await fetch("http://localhost:3000/posts", {
+      method: "DELETE"
+    });
+    console.log("Post deleted");
+  }
+  
+  deletePost();
+  
+  
+  
